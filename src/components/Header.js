d976d3b89c355d76a0504d81bd45fe9852a64c6d
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-07-25 00:05:55 -0400

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,13 @@
 const Header = (props) => {
 
     //The Components Returned JSX
-    return <div className="Header"> This is Header</div>
+    return <div className="Header">
+        <h1>Arielle Gironza</h1>
+        <ul>
+            <li><a href="https://github.com/akgironza">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/arielle-gironza/">LinkedIn</a></li>
+        </ul>
+    </div>
 }
 
 // export the component
